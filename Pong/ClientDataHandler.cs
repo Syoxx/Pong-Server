4e7130a4ccde87808ee9ebd433de8bbd9138ec9f
fSnow <it.icsti@googlemail.com>
2017-10-19 17:31:32 +0200

    Added client classes

---
+++ Pong/ClientDataHandler.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pong
+{
+    class ClientDataHandler
+    {
+        string game = "game";
+        string p1 = "p1";
+        string p2 = "p2";
+        string scoreP1 = "scoreP1";
+        string scoreP2 = "scoreP2";
+        string ball = "ball";
+
+
+        private string[] splitData;
+        string id;
+        float vector;
+        string p1VecX;
+        string p1VecY;
+        string p2VecX;
+        string p2VecY;
+        string ballVecX;
+        string ballVecY;
+        string scorePlayer1;
+        string scorePlayer2;
+
+        string pwdCorrect = "pwd accepted";
+        string content;
+
+        public string DataRequest(string scData, ThreadShareObject share)
+        {
+            if (scData.Contains(pwdCorrect))
+            {
+                splitData = scData.Split(' ');
+                id = splitData[0];
+            }
+
+            if (id == pwdCorrect)
+            {
+                Console.WriteLine("Password correct.");
+            }
+
+            if (scData.Contains(game))
+            {
+                content = splitData[1];
+
+                if (content == "p1")
+                {
+                    share.p1pos.X = Convert.ToSingle(p1VecX);
+                    p1VecX = splitData[2];
+
+                    share.p1pos.Y = Convert.ToSingle(p1VecY);
+                    p1VecY = splitData[3];
+                }
+                
+                else if (content == "p2")
+                {
+                    share.p2pos.X = Convert.ToSingle(p2VecX);
+                    p2VecX = splitData[2];
+
+                    share.p2pos.Y = Convert.ToSingle(p2VecY);
+                    p2VecY = splitData[3];
+                }
+
+                else if (content == "ball")
+                {
+                    share.ballPos.X = Convert.ToSingle(ballVecX);
+                    ballVecX = splitData[2];
+
+                    share.ballPos.X = Convert.ToSingle(ballVecY);
+                    ballVecY = splitData[3];
+                }
+
+                else if (content == "scoreP1")
+                {
+                    share.scoreP1 = Convert.ToChar(scorePlayer1); 
+                    scorePlayer1 = splitData[2];
+                }
+
+                else if (content == "scoreP2")
+                {
+                    share.scoreP2 = Convert.ToChar(scorePlayer2);
+                    scorePlayer2 = splitData[2];
+                }
+            }
+
+
+
+        }
+    }
+}
---
+++ Pong/ClientPlayer.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pong
+{
+    /// <summary>
+    /// Every input comming from client.
+    /// </summary>
+    class ClientPlayer
+    {
+        string pwd = "pwd";
+		string cmd = "cmd";
+		string rData = "cmd accepted";
+		string eData = "cmd error";
+		string identifier;
+		private string content;
+
+		private string[] splitData;
+
+        string inputPwd;
+
+		public string RequestData(string sData, ThreadShareObject share)
+		{
+            if (ClientConnectionUp == true)
+            {
+                Console.WriteLine("Enter password to connect a game: " + inputPwd);
+
+            }
+
+            //server receives a password from the client
+            if (identifier == pwd)
+			{
+				if (content == password)
+				{
+					share.PwdAccepted = true;
+					return correct;
+				}
+
+				return incorrect;
+			}
+
+			//server receives a cmd from the client
+			 if (identifier == cmd)
+			{
+				//client sent up command, moves the player object up
+				if (content == "up")
+				{
+					share.Up = true;
+					rData = "cmd accepted";
+				}
+				//client sent down command, moces the player object down
+				else if (content == "down")
+				{
+					//SendKeys.Send(Keys.Down.ToString());
+					share.Down = true;
+					rData = "cmd accepted";
+				}
+
+				else if (content == "update")
+				{
+					rData = "cmd update";
+				}
+
+				else
+				{
+					rData = "cmd wrong";
+				}
+
+				return rData;
+			}
+
+			return eData;
+		}
+    }
+}
